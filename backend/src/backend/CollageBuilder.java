b192fc5228df85098772300371a9e03c1d5f06dd
Sahil Agarwal <sahilagarwal@Sahils-MacBook-Pro.local>
2018-02-23 14:59:09 -0800

    Rotate image, and random angle methods

--- backend/src/backend/CollageBuilder.java
+++ backend/src/backend/CollageBuilder.java
@@ -1,9 +1,11 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class CollageBuilder {
 	
-	private Boolean insufficientErrorMessageBoolean;
 	private List<Collage> collages;
 	
 	public CollageBuilder() {
@@ -19,6 +21,26 @@ public class CollageBuilder {
 	//builds the collage
 	public void buildCollage() {
 		
+	}
+	
+	
+	//rotates a given image with an angle
+	public static BufferedImage rotate(BufferedImage img, int angle) {  
+        int w = img.getWidth();  
+        int h = img.getHeight();  
+		BufferedImage dimg = new BufferedImage(w, h, img.getType());  
+        Graphics2D g = dimg.createGraphics();  
+        g.rotate(Math.toRadians(angle), w/2, h/2);  
+        g.drawImage(img, null, 0, 0);  
+        return dimg;  
+    }  
+	
+	//generates a random angle
+	private int generateRandomAngle() {
+		Random rand = new Random();
+		int  n = rand.nextInt(91) - 45; //generates a random number 0-90, and scales it to be from -45 to 45
+		
+		return n;
 	}
 	
 	
