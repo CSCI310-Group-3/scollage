1233c43f72ad1d1827517f2b70fd9f72756780e7
Sahil Agarwal <sahilagarwal@Sahils-MacBook-Pro.local>
2018-02-23 14:45:12 -0800

    Right version

--- backend/src/backend/Collage.java
+++ backend/src/backend/Collage.java
@@ -1,4 +1,54 @@
+import java.awt.Image;
+import java.net.URL;
+
+import javax.swing.ImageIcon;
 
 public class Collage {
+	
+	private String title;
+	private URL imageURL;
+	private Boolean display;
+	private Image image; //obtained from the url passed in constructor
+	
+	public Collage(String title, URL imageURL, Boolean display) {
+		this.title = title;
+		this.imageURL = imageURL;
+		this.display = display;
+		
+		ImageIcon icon = new ImageIcon(imageURL);
+		this.image = icon.getImage();
+	}
+	
+	
+	//exports the image, not sure how to implement
+	public void export() {
+		
+	}
+	
+	//scales the collage, not sure how to implem,ent
+	public void scale() {
+		
+		
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public URL getImageURL() {
+		return imageURL;
+	}
+
+
+	public Boolean getDisplay() {
+		return display;
+	}
+
+
+	public Image getImage() {
+		return image;
+	}
 
 }
